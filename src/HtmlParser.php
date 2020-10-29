e0643fd7f11fd01474b1480dd83b81aca669bae8
Roman Opalko <roman.opalko@gmail.com>
2020-10-29 21:20:48 +0200

    first commit

---
+++ src/HtmlParser.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Opalkor\HtmlParser;
+
+
+class HtmlParser {
+
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
