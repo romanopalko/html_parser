ca7952f81a1156996a896c732b0cfe8a6f0e4cec
Roman Opalko <roman.opalko@gmail.com>
2020-10-29 21:30:11 +0200

    update methods

--- src/HtmlParser.php
+++ src/HtmlParser.php
@@ -3,11 +3,21 @@
 
 namespace Opalkor\HtmlParser;
 
+require_once 'simple_html_dom.php';
 
 class HtmlParser {
 
-    public function greet(String $sName)
-    {
-        return 'Hi ' . $sName . '! How are you doing today?';
+    /**
+     * @return \simple_html_dom.php
+     */
+    static public function file() {
+        return call_user_func_array ( '\file_get_html' , func_get_args() );
+    }
+
+    /**
+     * @return \simple_html_dom.php
+     */
+    static public function string() {
+        return call_user_func_array ( '\str_get_html' , func_get_args() );
     }
 }
